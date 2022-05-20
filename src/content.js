70fb532b621217c8793e5c18c12120f605da8b9a
raf-underscore <salajaraphael@gmail.com>
2022-05-20 22:52:54 +0100

    test: 🧪 remade ui

--- src/content.js
+++ src/content.js
@@ -11,33 +11,26 @@ $(document).ready(() => {
 		console.log('open_unite')
 
 		$('#unite-extension').removeClass('unite-hidden')
-
-		$(function () {
-			$('#unite-extension').draggable({
-				handle: '#unite-header',
-				addClasses: true,
-				appendTo: 'body',
-				iframeFix: true,
-				opacity: 0.5,
-			})
-		})
-
-		$('#unite-extension #logo ').attr('src', chrome.runtime.getURL('assets/layers.svg'))
-		$('#unite-extension #links ').attr('src', chrome.runtime.getURL('assets/mug-hot.svg'))
-		$('#unite-extension #settings ').attr('src', chrome.runtime.getURL('assets/settings-sliders.svg'))
-		$('#unite-extension #close ').attr('src', chrome.runtime.getURL('assets/cross-circle.svg'))
-
-		const topbar_ids = ['#unite-extension #logo', '#unite-extension #links', '#unite-extension #settings', '#unite-extension #close']
-		topbar_ids.forEach((id) => {
-			$(id).svgInject(function () {
-				// Injection complete
-			})
-		})
 	}
-	function close_unite() {
-		unite_open = false
-		$('#unite-extension').addClass('unite-hidden')
-	}
+
+	// 	$('#unite-extension #logo ').attr('src', chrome.runtime.getURL('assets/layers.svg'))
+	// 	$('#unite-extension #links ').attr('src', chrome.runtime.getURL('assets/mug-hot.svg'))
+	// 	$('#unite-extension #settings ').attr('src', chrome.runtime.getURL('assets/settings-sliders.svg'))
+	// 	$('#unite-extension #close ').attr('src', chrome.runtime.getURL('assets/cross-circle.svg'))
+
+	// 	const topbar_ids = ['#unite-extension #logo', '#unite-extension #links', '#unite-extension #settings', '#unite-extension #close']
+	// 	topbar_ids.forEach((id) => {
+	// 		$(id).svgInject(function () {})
+	// 	})
+
+	// 	$('#search').selectize({
+	// 		sortField: 'text',
+	// 	})
+	// }
+	// function close_unite() {
+	// 	unite_open = false
+	// 	$('#unite-extension').addClass('unite-hidden')
+	// }
 
 	chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 		switch (request.message) {
