5515939009bed8ad2b583f7d8dcb2f4a000c5ee1
raf-underscore <salajaraphael@gmail.com>
2022-05-25 21:55:08 +0100

    feat: 🔥 quick commands are working, now for cleanup and search

--- src/background.js
+++ src/background.js
@@ -1,44 +1,210 @@
-// set variables
-// icon, title, type, description, shortcut
-let quick_commands = [
-	{
-		icon: '',
-		title: '',
-		type: '',
-		description: '',
-		shortcut: '',
-		action: '',
-	},
-]
+let quick_commands = []
 
-// set quick commands
-const setQuickCommands = () => {
-	getActiveTab().then((response) => {
-		addTabs()
+const set_command_list = () => {
+	get_active_tab().then((response) => {
+		quick_commands = []
+		quick_commands = [
+			{icon: '', title: 'Open in new tab', type: 'Action', description: 'Open the current page in a new tab', shortcut: 'Ctrl + T', action: 'new-tab'},
+			{icon: '', title: 'Open in new window', type: 'Action', description: 'Open the current page in a new window', shortcut: 'Ctrl + W', action: 'new-window'},
+			{icon: '', title: 'Open in incognito window', type: 'Action', description: 'Open the current page in an incognito window', shortcut: 'Ctrl + Shift + W', action: 'incognito-window'},
+			{icon: '', title: 'Next Tab', type: 'Action', description: 'Go to the next tab', shortcut: 'Ctrl + Tab', action: 'next-tab'},
+			{icon: '', title: 'Previous Tab', type: 'Action', description: 'Go to the previous tab', shortcut: 'Ctrl + Shift + Tab', action: 'previous-tab'},
+			{icon: '', title: 'Close Tab', type: 'Action', description: 'Close the current tab', shortcut: 'Ctrl + W', action: 'close-tab'},
+			{icon: '', title: 'Close Window', type: 'Action', description: 'Close the current window', shortcut: 'Ctrl + Shift + W', action: 'close-window'},
+			{icon: '', title: 'Add to Bookmarks', type: 'Action', description: 'Add the current page to bookmarks', shortcut: 'Ctrl + D', action: 'add-bookmark'},
+			{icon: '', title: 'Clear Cache', type: 'Action', description: 'Clear the cache', shortcut: 'Ctrl + Shift + C', action: 'clear-cache'},
+			{icon: '', title: 'Clear History', type: 'Action', description: 'Clear the history', shortcut: 'Ctrl + Shift + H', action: 'clear-history'},
+			{icon: '', title: 'Clear Cookies', type: 'Action', description: 'Clear the cookies', shortcut: 'Ctrl + Shift + K', action: 'clear-cookies'},
+			{icon: '', title: 'Clear All', type: 'Action', description: 'Clear all data', shortcut: 'Ctrl + Shift + A', action: 'clear-all'},
+		]
+		get_tabs()
 	})
 }
-
-// functions
-const getActiveTab = async () => {
-	return await chrome.tabs.query({active: true, currentWindow: true})
-}
-const addTabs = async () => {
+const get_tabs = () => {
 	chrome.tabs.query({}, function (tabs) {
 		for (let i = 0; i < tabs.length; i++) {
+			let url = tabs[i].url
+			url = url.substring(0, url.lastIndexOf('/'))
 			quick_commands.push({
 				icon: tabs[i].favIconUrl,
 				title: tabs[i].title,
 				type: 'Tabs',
-				description: tabs[i].url,
+				description: url,
 				shortcut: '',
 				action: 'switch-tab',
+				tab: tabs[i],
 			})
 		}
 	})
 }
+const get_active_tab = async () => {
+	return await chrome.tabs.query({active: true, currentWindow: true})
+}
 
-// inject all scripts on install
+chrome.action.onClicked.addListener((tab) => {
+	chrome.tabs.sendMessage(tab.id, {message: 'open-unite'}, function (response) {})
+})
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	switch (message.request) {
+		case 'get-quick-commands':
+			sendResponse({quick_commands: quick_commands})
+			break
+		case 'switch-tab':
+			// defocus from current tab and focus on new tab
+			chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+				chrome.tabs.update(tabs[0].id, {active: false})
+				chrome.tabs.update(message.tab.id, {active: true})
+			})
+
+			break
+		case 'close-tab':
+			chrome.tabs.getCurrent(function (tab) {
+				chrome.tabs.remove(tab.id, function () {})
+			})
+			break
+		case 'new-tab':
+			chrome.tabs.create({url: 'chrome://newtab'})
+			break
+		case 'new-window':
+			chrome.windows.create({url: 'chrome://newtab'})
+			break
+		case 'incognito-window':
+			chrome.windows.create({url: 'chrome://newtab', incognito: true})
+			break
+		case 'next-tab':
+			chrome.tabs.query({currentWindow: true}, function (tabs) {
+				var index = tabs.findIndex(function (tab) {
+					return tab.active
+				})
+				chrome.tabs.update(tabs[(index + 1) % tabs.length].id, {active: true})
+			})
+			break
+		case 'previous-tab':
+			chrome.tabs.query({currentWindow: true}, function (tabs) {
+				var index = tabs.findIndex(function (tab) {
+					return tab.active
+				})
+				chrome.tabs.update(tabs[(index - 1 + tabs.length) % tabs.length].id, {active: true})
+			})
+			break
+		case 'close-window':
+			chrome.windows.getCurrent(function (window) {
+				chrome.windows.remove(window.id, function () {})
+			})
+			break
+		case 'add-bookmark':
+			chrome.tabs.getCurrent(function (tab) {
+				chrome.bookmarks.create({
+					title: tab.title,
+					url: tab.url,
+				})
+			})
+			break
+		case 'clear-cache':
+			chrome.browsingData.remove(
+				{
+					since: 0,
+				},
+				{
+					appcache: true,
+					cache: true,
+					cacheStorage: true,
+					cookies: true,
+					downloads: true,
+					fileSystems: true,
+					formData: true,
+					history: true,
+					indexedDB: true,
+					localStorage: true,
+					pluginData: true,
+					passwords: true,
+					webSQL: true,
+				},
+				function () {}
+			)
+			break
+		case 'clear-history':
+			chrome.browsingData.remove(
+				{
+					since: 0,
+				},
+				{
+					appcache: true,
+					cache: true,
+					cacheStorage: true,
+					cookies: true,
+					downloads: true,
+					fileSystems: true,
+					formData: true,
+					history: true,
+					indexedDB: true,
+					localStorage: true,
+					pluginData: true,
+					passwords: true,
+					webSQL: true,
+				},
+				function () {}
+			)
+			break
+		case 'clear-cookies':
+			chrome.browsingData.remove(
+				{
+					since: 0,
+				},
+				{
+					appcache: true,
+					cache: true,
+					cacheStorage: true,
+					cookies: true,
+					downloads: true,
+					fileSystems: true,
+					formData: true,
+					history: true,
+					indexedDB: true,
+					localStorage: true,
+					pluginData: true,
+					passwords: true,
+					webSQL: true,
+				},
+				function () {}
+			)
+			break
+		case 'clear-all':
+			chrome.browsingData.remove(
+				{
+					since: 0,
+				},
+				{
+					appcache: true,
+					cache: true,
+					cacheStorage: true,
+					cookies: true,
+					downloads: true,
+					fileSystems: true,
+					formData: true,
+					history: true,
+					indexedDB: true,
+					localStorage: true,
+					pluginData: true,
+					passwords: true,
+					webSQL: true,
+					serviceWorkers: true,
+					cacheStorage: true,
+					appcache: true,
+				},
+				function () {}
+			)
+			break
+		case 'open-settings':
+			chrome.runtime.openOptionsPage()
+			break
+	}
+})
 chrome.runtime.onInstalled.addListener((object) => {
+	// clear console log
+	console.clear()
+	set_command_list()
+
 	const injectScripts = (tab) => {
 		const manifest = chrome.runtime.getManifest()
 		const scripts = manifest.content_scripts[0].js
@@ -80,33 +246,4 @@ chrome.runtime.onInstalled.addListener((object) => {
 			}
 		}
 	)
-
-	console.clear()
-	console.group('Installation | Unite Extension')
-	console.info('Unite Extension Installed')
 })
-// messaging
-// sending
-chrome.action.onClicked.addListener((tab) => {
-	setQuickCommands()
-	chrome.tabs.sendMessage(tab.id, {message: 'open-unite'}, function (response) {})
-})
-// receive
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-	switch (message.request) {
-		case 'get-quick-commands':
-			setQuickCommands()
-			sendResponse(quick_commands)
-			break
-	}
-})
-
-// Quick Commands
-// switch tab
-const switchTab = (tab) => {
-	chrome.tabs.highlight({
-		tabs: tab.index,
-		windowId: tab.windowId,
-	})
-	chrome.windows.update(tab.windowId, {focused: true})
-}
--- src/content.js
+++ src/content.js
@@ -11,33 +11,96 @@ $(document).ready(() => {
 		console.log('open_unite')
 		$('#unite-extension').fadeIn()
 		$('#unite-extension').removeClass('unite-hidden')
+		set_command_list()
 	}
 	function close_unite() {
 		unite_open = false
 		$('#unite-extension').addClass('unite-hidden')
 		$('#unite-extension').fadeOut()
 	}
+	function set_command_list() {
+		chrome.runtime.sendMessage({request: 'get-quick-commands'}, (response) => {
+			for (let i = 0; i < response.quick_commands.length; i++) {
+				let quick_command = response.quick_commands[i]
+				let $unite_search_results = $('<div>', {
+					'id': 'unite-search-results',
+					'data-id': i,
+					'data-action': quick_command.action,
+				})
+				let $unite_search_results_container = $('<div>', {
+					id: 'unite-search-results-container',
+				})
+				let $unite_search_results_icon_container = $('<div>', {
+					id: 'unite-search-results-icon-container',
+				})
 
-	// 	$('#unite-extension #logo ').attr('src', chrome.runtime.getURL('assets/layers.svg'))
-	// 	$('#unite-extension #links ').attr('src', chrome.runtime.getURL('assets/mug-hot.svg'))
-	// 	$('#unite-extension #settings ').attr('src', chrome.runtime.getURL('assets/settings-sliders.svg'))
-	// 	$('#unite-extension #close ').attr('src', chrome.runtime.getURL('assets/cross-circle.svg'))
+				let $unite_search_results_icon = $('<img>', {
+					id: 'unite-search-results-icon',
+				})
 
-	// 	const topbar_ids = ['#unite-extension #logo', '#unite-extension #links', '#unite-extension #settings', '#unite-extension #close']
-	// 	topbar_ids.forEach((id) => {
-	// 		$(id).svgInject(function () {})
-	// 	})
-
-	// 	$('#search').selectize({
-	// 		sortField: 'text',
-	// 	})
-	// }
+				let $unite_search_results_placeholder = $('<div>', {
+					id: 'unite-search-results-placeholder',
+				})
+				let $unite_search_results_text_container = $('<div>', {
+					id: 'unite-search-results-text-container',
+				})
+				let $unite_search_results_text_main = $('<span>', {
+					id: 'unite-search-results-text-main',
+					text: quick_command.title,
+				})
+				let $unite_search_results_details_container = $('<div>', {
+					id: 'unite-search-results-details-container',
+				})
+				let $unite_search_results_results_badge = $('<div>', {
+					id: 'unite-search-results-results-badge',
+				})
+				let $unite_search_results_results_badge_text = $('<div>', {
+					id: 'unite-search-results-results-badge-text',
+					text: quick_command.type,
+				})
+				let $unite_search_results_details_text = $('<div>', {
+					id: 'unite-search-results-details-text',
+					text: quick_command.description,
+				})
+				if (quick_command.icon != '') {
+					$unite_search_results_icon_container.append($unite_search_results_icon)
+					$unite_search_results_icon.attr('src', quick_command.icon)
+				} else {
+					$unite_search_results_icon_container.append($unite_search_results_placeholder)
+				}
+				$unite_search_results_results_badge.append($unite_search_results_results_badge_text)
+				$unite_search_results_details_container.append($unite_search_results_results_badge)
+				$unite_search_results_details_container.append($unite_search_results_details_text)
+				$unite_search_results_text_container.append($unite_search_results_details_container)
+				$unite_search_results_text_container.append($unite_search_results_text_main)
+				$unite_search_results_container.append($unite_search_results_icon_container)
+				$unite_search_results_container.append($unite_search_results_text_container)
+				$unite_search_results.append($unite_search_results_container)
+				$('#unite-search-contents').append($unite_search_results)
+			}
+		})
+	}
+	function hover_result() {
+		$(this).addClass('unite-search-results-hover')
+		$(this).on('mouseout', () => {
+			$(this).removeClass('unite-search-results-hover')
+		})
+	}
+	function execute_action() {
+		let action = $(this).attr('data-action')
+		// find which search result was clicked
+		let id = $(this).attr('data-id')
+		// get the tab data of the result that was click by checking it in the quick_commands array
+		chrome.runtime.sendMessage({request: 'get-quick-commands'}, (response) => {
+			// get the tab of the result that was click by checking it in the quick_commands array
+			let tab = response.quick_commands[id].tab
+			// send the action to the background script
+			chrome.runtime.sendMessage({request: action, tab: tab})
+		})
+	}
 
 	chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 		switch (request.message) {
-			case 'test-unite':
-				sendResponse({return: 'Connection Successful'})
-				break
 			case 'open-unite':
 				unite_open ? close_unite() : open_unite()
 				sendResponse({return: 'unite opened'})
@@ -49,15 +112,7 @@ $(document).ready(() => {
 		}
 		return true
 	})
-	chrome.runtime.sendMessage({request: 'get-quick-commands'}, (response) => {
-		quick_commands = response.quick_commands
-	})
-})
 
-//   $('#field').autocomplete({
-// 		source: countries_starting_with_A,
-// 		minLength: 0,
-// 		close: function (event, ui) {
-// 			$input.autocomplete('widget').show()
-// 		},
-//   })
+	$(document).on('mouseover', '#unite-search-results', hover_result)
+	$(document).on('click', '#unite-search-results', execute_action)
+})
